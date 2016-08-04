a309ca58d301f45c4a3de6372c605dca00268315
hanhan1978 <ryo.tomidokoro@gmail.com>
2016-08-05 00:42:32 +0900

    issue #1, #2

--- pro.php
+++ pro.php
@@ -2,12 +2,24 @@
 
 class Profiler {
 
+    private $name;
     private $probe_points = [];
     private $probe_lines = [];
     private $log_file = '/tmp/profiler.log';
+    private $disabled = false;
+
+    public function __construct($name = '')
+    {
+        $this->name = $name;
+    }
+    public function disable()
+    {
+        $this->disabled = true;
+    }
 
     public function probe($bt = null)
     {
+        if($this->disabled) return;
         if(is_null($bt)){
           $bt = debug_backtrace();
         }
@@ -17,14 +29,15 @@ class Profiler {
 
     public function probe_end()
     {
+        if($this->disabled) return;
         $this->probe(debug_backtrace());
 
         $str = $_SERVER['REQUEST_URI']."\t";
         $str .= $_SERVER['REQUEST_METHOD']."\t";
         for($i=0; $i<count($this->probe_points) ; $i++){
-          $str .= $this->probe_lines[$i]."\t";
+          $str .= "L".$this->probe_lines[$i]."\t";
           if($i < (count($this->probe_points) -1)){
-            $str .= sprintf("%f\t", $this->probe_points[$i+1] - $this->probe_points[$i]); 
+            $str .= sprintf("%f\t", $this->probe_points[$i+1] - $this->probe_points[$i]);
           }
         }
         $this->write_log($str);
@@ -32,11 +45,9 @@ class Profiler {
 
     private function write_log($str)
     {
+        $str = "[".$this->name."] ".$str;
         $fp = fopen($this->log_file, 'a');
         fwrite($fp, $str."\n");
         fclose($fp);
     }
 }
-
-
-
